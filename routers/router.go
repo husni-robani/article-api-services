a203fe827b31d26791fb299ae8f2c562d441f6d7
husni-robani <husnir2005@gmail.com>
2024-11-04 22:34:29 +0700

    add auth route and create new article

--- routers/router.go
+++ routers/router.go
@@ -3,12 +3,23 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/husni-robani/article-api-services/controllers"
+	"github.com/husni-robani/article-api-services/middleware"
 )
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
 
-	router.GET("/api/articles", controllers.GetAllArticles)
-
+	api := router.Group("/api")
+	{
+		authenticatedRoute := api.Group("")
+		authenticatedRoute.Use(middleware.AuthMiddleware())
+		{
+			authenticatedRoute.POST("/articles", controllers.CreateArticle)
+		}
+		
+		api.POST("/register", controllers.Register)
+        api.POST("/login", controllers.Login)
+		api.GET("/articles", controllers.GetAllArticles)
+	}
 	return router
 }
