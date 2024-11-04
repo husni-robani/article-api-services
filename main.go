ef83f3226bd5190477cdca0a9c19891328e4e5da
husni-robani <husnir2005@gmail.com>
2024-11-04 19:20:45 +0700

    setup main.go

--- main.go
+++ main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import (
+	"github.com/husni-robani/article-api-services/config"
+	"github.com/husni-robani/article-api-services/routers"
+)
+
+func main() {
+	config.ConnectDatabase()	
+
+	router := routers.SetupRouter()
+	router.Run(":8080")
+}
