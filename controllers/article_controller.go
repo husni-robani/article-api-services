60ac60102b65dd63f3f7e78141191592f13cbbe3
husni-robani <husnir2005@gmail.com>
2024-11-05 00:45:21 +0700

    create controller function for update and delete article

--- controllers/article_controller.go
+++ controllers/article_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/husni-robani/article-api-services/config"
@@ -37,9 +38,15 @@ func GetAllArticles(c *gin.Context){
 }
 
 func CreateArticle(c *gin.Context){
-	var new_article models.Article
-	err := c.ShouldBindJSON(&new_article)
+	author_id, err := strconv.Atoi(c.Param("author_id"))
 	if err != nil{
+		response.Error(c, http.StatusBadRequest, "Invalid Author ID", err.Error())
+		return
+	}
+
+	var new_article models.Article
+	new_article.AuthorId = author_id
+	if err := c.ShouldBindJSON(&new_article); err != nil{
 		response.Error(c, http.StatusBadRequest, "Failed to create new article", err.Error())
 		return
 	}
@@ -59,4 +66,55 @@ func CreateArticle(c *gin.Context){
 	new_article.Id = int(id)
 
 	response.Success(c, http.StatusCreated, new_article, "Article created successfully!!")
+}
+
+func DeleteArticle(c *gin.Context){
+	
+	article_id, err := strconv.Atoi(c.Param("article_id"))
+	if err != nil {
+		response.Error(c, http.StatusBadRequest, "Invalid Article Id", err.Error())
+		return
+	}
+
+	statement, err := config.DB.Prepare("DELETE FROM articles WHERE id = ?")
+	if err != nil{
+		response.Error(c, http.StatusInternalServerError, "Failed to prepare statment", err.Error())
+		return
+	}
+
+	_, err = statement.Exec(article_id)
+	if err != nil{
+		response.Error(c, http.StatusInternalServerError, "Failed to execute statement", err.Error())
+		return
+	}
+
+	response.Success(c, http.StatusOK, nil, "Article Deleted Successfull")
+}
+
+func UpdateArticle(c *gin.Context){
+	article_id, err := strconv.Atoi(c.Param("article_id"))
+	if err != nil {
+		response.Error(c, http.StatusBadRequest, "Invalid Article Id", err.Error())
+		return 
+	}
+	
+	var article models.Article
+	if err := c.ShouldBindBodyWithJSON(&article); err != nil {
+		response.Error(c, http.StatusBadRequest, "Invalid Input", err.Error())
+		return 
+	}
+
+	statement, err := config.DB.Prepare("UPDATE articles SET title = ?, content = ?, category_id = ? WHERE id = ?")
+	if err != nil{
+		response.Error(c, http.StatusInternalServerError, "Failed to prepare statement", err.Error())
+		return
+	}
+
+	_, err = statement.Exec(article.Title, article.Content, article.CategoryId, article_id)
+	if err != nil{
+		response.Error(c, http.StatusInternalServerError, "Failed to Execute Statement", err.Error())
+		return
+	}
+	article.Id = article_id
+	response.Success(c, http.StatusOK, article, "Article Updated Successfull")
 }
