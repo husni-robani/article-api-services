0f5cd0f9cc193e7ae93e2d14d3cd0dc21a0c40b8
husni-robani <husnir2005@gmail.com>
2024-11-04 21:58:01 +0700

    create middleware for authentication

---
+++ middleware/auth.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/husni-robani/article-api-services/utils"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        authHeader := c.GetHeader("Authorization")
+        if authHeader == "" {
+            c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header required"})
+            c.Abort()
+            return
+        }
+
+        tokenString := strings.Split(authHeader, "Bearer ")[1]
+		fmt.Println("Token: ", tokenString)
+        claims, err := utils.ValidateJWT(tokenString)
+        if err != nil {
+            c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+            c.Abort()
+            return
+        }
+
+        c.Set("userID", claims.UserID)
+        c.Next()
+    }
+}
