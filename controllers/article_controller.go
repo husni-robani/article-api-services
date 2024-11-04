ead732a4686d6007522780ac4533d0823f8e3254
husni-robani <husnir2005@gmail.com>
2024-11-04 20:11:09 +0700

    modify response format

--- controllers/article_controller.go
+++ controllers/article_controller.go
@@ -6,13 +6,14 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/husni-robani/article-api-services/config"
 	"github.com/husni-robani/article-api-services/models"
+	"github.com/husni-robani/article-api-services/response"
 )
 
 func GetAllArticles(c *gin.Context){
 	rows, err := config.DB.Query("SELECT * FROM articles")	
 
 	if err != nil{
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		response.Error(c, http.StatusInternalServerError, "Failed to get articles", err.Error())
 		return
 	}
 
@@ -25,12 +26,12 @@ func GetAllArticles(c *gin.Context){
 		err := rows.Scan(&article.Id, &article.Title, &article.Content, &article.AuthorId, &article.CategoryId, &article.CreatedAt, &article.UpdatedAt)
 
 		if err != nil{
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			response.Error(c, http.StatusInternalServerError, "Failed to scan articles", err.Error())
 			return
 		}
 
 		articles = append(articles, article)
 	}
 
-	c.JSON(http.StatusOK, articles)
+	response.Success(c, http.StatusOK, articles, "Success to get articles")
 }
