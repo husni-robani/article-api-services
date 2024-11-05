1cc4377e2e90bf7a79d37c8c42f0f1acbc479226
husni-robani <husnir2005@gmail.com>
2024-11-05 10:39:25 +0700

    add router to send api docs URL

--- routers/router.go
+++ routers/router.go
@@ -1,9 +1,12 @@
 package routers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/husni-robani/article-api-services/controllers"
 	"github.com/husni-robani/article-api-services/middleware"
+	"github.com/husni-robani/article-api-services/response"
 )
 
 func SetupRouter() *gin.Engine {
@@ -27,6 +30,11 @@ func SetupRouter() *gin.Engine {
 		api.POST("/register", controllers.Register)
         api.POST("/login", controllers.Login)
 		api.GET("/articles", controllers.GetAllArticles)
+
+		// endpoint that gives documentation URL in response
+		api.GET("/docs", func (c *gin.Context){
+			response.Success(c, http.StatusOK, gin.H{"documentation_url": "https://documenter.getpostman.com/view/26114103/2sAY4yeLhF"}, "Success")
+		})
 	}
 	return router
 }
